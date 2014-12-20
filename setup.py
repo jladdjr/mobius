f25a80172e19a1cbb30425eb9cdca27058de3159
James Ladd Jr <mobius.search@gmail.com>
2014-12-20 01:26:35 -0500

    MOB-2: Modified setup.py

--- setup.py
+++ setup.py
@@ -7,20 +7,22 @@ Call distutils to install Mobius.
 from distutils.core import setup
 
 setup(
-    name='Mobius',
+    name='mobius',
     version='0.0.1',
     author='James Ladd Jr',
     author_email='mobius.search@gmail.com',
     packages=['mobius', 'mobius.test'],
     url='https://github.com/jladdjr/mobius',
-    license='LICENSE.txt',
+    license='GPLv2',
     description='A micro search engine',
     long_description=open('README.txt').read(),
     classifiers=[
         "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
         "Development Status :: 1 - Planning"
-        ]
+        ],
+    keywords='web search',
+    install_requires=['unittest', 'xmlrunner']
 )
